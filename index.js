6ebf72842934cd0cbc6740f3e93273f7776e24dd
marvokdolor <marvourneen@mkdolor.com>
2016-12-04 15:34:51 -0500

    completed first version of viewer

--- index.js
+++ index.js
@@ -1,28 +1,35 @@
 $(document).ready(function(){
-  var usernames = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "noobs2ninjas", "RobotCaleb"];
+  var usernames = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "noobs2ninjas", "RobotCaleb", "brunofin", "comster404"];
   for(var i = 0; i < usernames.length; i++){
     var user = usernames[i];
 
     $.getJSON('https://wind-bow.hyperdev.space/twitch-api/streams/'+usernames[i]+'?callback=?', function(data) {
-      var status = data['stream'];
-      var channel =  data['_links']['channel'];
-      var channelSplit = channel.split('/');
-      var channelName = channelSplit[5];
-      console.log(data);
-      console.log('status: ' + status);
-      console.log('user: ' + user);
-      console.log(channelSplit);
-      console.log(channelName);
-      if (status == null) {
-        //document.getElementById('status').innerHTML = "offline";
-        $('#link').prepend('<div><p>Offline</p></div>');
+      if(data['error']){
+        var message = data['message'];
+        $('#link').prepend('<div><p>'+message+'</p></div>');
       }
       else{
-        var link = status.channel.url;
-        var name = status.channel.display_name;
-        //document.getElementById('status').innerHTML = "online";
-        $('#link').prepend('<div><p>Online</p><a href = '+link+'>'+name+'</a></div>');
+        var status = data['stream'];
+        var channel =  data['_links']['channel'];
+        var channelSplit = channel.split('/');
+        var channelName = channelSplit[5];
+        console.log(data);
+        console.log('status: ' + status);
+        console.log('user: ' + user);
+        if (status == null) {
+          $('#link').prepend('<div><p>Offline</p>'+channelName+'</div><hr />');
+        }
+        else{
+          var link = status.channel.url;
+          var name = status.channel.display_name;
+          var game = status.game;
+          var viewers = status.viewers;
+
+          $('#link').prepend('<div><p>Online<br /><a href="'+link+'" target="_blank">'+name+'</a><br />What\'s streaming: '+game+'<br /> Number of viewers: '+viewers+'</p></div>');
+        }
       }
+    }).fail(function() {
+      $('#link').prepend('<div><p>Account doesn\'t exist');
     });
   }
 })
