f96cd5a443e62f81fe2e1fa65e41de381a8d73fb
marvokdolor <marvourneen@mkdolor.com>
2016-12-04 14:52:18 -0500

    Accessed Channel Names with a new variable

--- index.js
+++ index.js
@@ -5,21 +5,28 @@ $(document).ready(function(){
 
     $.getJSON('https://wind-bow.hyperdev.space/twitch-api/streams/'+usernames[i]+'?callback=?', function(data) {
       var status = data['stream'];
-
+      var channel =  data['_links']['channel'];
+      var channelSplit = channel.split('/');
+      var channelName = channelSplit[5];
       console.log(data);
       console.log('status: ' + status);
       console.log('user: ' + user);
+      console.log(channelSplit);
+      console.log(channelName);
       if (status == null) {
         //document.getElementById('status').innerHTML = "offline";
         $('#link').prepend('<div><p>Offline</p></div>');
       }
       else{
         var link = status.channel.url;
+        var name = status.channel.display_name;
         //document.getElementById('status').innerHTML = "online";
-        $('#link').prepend('<div><p>Online</p><a href = '+link+'>'+user+'</a></div>');
+        $('#link').prepend('<div><p>Online</p><a href = '+link+'>'+name+'</a></div>');
       }
     });
   }
 })
 
-//{stream: null, _links: {self: "https://api.twitch.tv/kraken/streams/freecodecamp", channel: "https://api.twitch.tv/kraken/channels/freecodecamp"}}
+ /*{stream: null,
+  _links:
+    {self: "https://api.twitch.tv/kraken/streams/freecodecamp", channel: "https://api.twitch.tv/kraken/channels/freecodecamp"}}*/
