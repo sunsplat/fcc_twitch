ae45e4882ea470f16042cd1c23eb0ced5a71b52d
marvokdolor <marvourneen@mkdolor.com>
2016-12-04 14:03:51 -0500

    added an array of users for api call

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 $(document).ready(function(){
-
-  $.getJSON('https://wind-bow.hyperdev.space/twitch-api/streams/freecodecamp?callback=?', function(data) {
+  var usernames = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+  for(var i = 0; i < usernames.length; i++){
+    var user = usernames[i];
+  $.getJSON('https://wind-bow.hyperdev.space/twitch-api/streams/'+user+'?callback=?', function(data) {
     var status = data['stream'];
-    var link = status.channel.url;
     console.log(data);
     console.log('status: ' + status);
     if (status == null) {
       document.getElementById('status').innerHTML = "offline";
     }
     else{
+      var link = status.channel.url;
       document.getElementById('status').innerHTML = "online";
-      document.getElementById('link').innerHTML = "<a href></a>";
+      document.getElementById('link').innerHTML = '<a href = '+link+'>'+user+'</a>';
     }
   });
+}
 })
 
 //{stream: null, _links: {self: "https://api.twitch.tv/kraken/streams/freecodecamp", channel: "https://api.twitch.tv/kraken/channels/freecodecamp"}}
