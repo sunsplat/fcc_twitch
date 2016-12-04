7cd44c499fa6c751d77044290302893eac5a8e69
marvokdolor <marvourneen@mkdolor.com>
2016-12-04 13:39:33 -0500

    started api functionality

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ $(document).ready(function(){
 
   $.getJSON('https://wind-bow.hyperdev.space/twitch-api/streams/freecodecamp?callback=?', function(data) {
     var status = data['stream'];
+    var link = status.channel.url;
     console.log(data);
     console.log('status: ' + status);
     if (status == null) {
       document.getElementById('status').innerHTML = "offline";
+    }
+    else{
+      document.getElementById('status').innerHTML = "online";
+      document.getElementById('link').innerHTML = "<a href></a>";
     }
   });
 })
