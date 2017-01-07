83609b7625b2fb5d61697c8851ccddc186aa674e
Ellen Sun <heeycutie@yahoo.com>
2017-01-07 15:56:10 -0500

    converted jquery ajax call to xmlhttprequest

--- index.js
+++ index.js
@@ -1,41 +1,74 @@
-$(document).ready(function(){
-  var usernames = ["solasce", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "boris_chantel", "RobotCaleb", "brunofin", "comster404"];
-  for (var i = 0; i < usernames.length; i++) {
-     var user = usernames[i];
-     $.getJSON('https://wind-bow.gomix.me/twitch-api/streams/'+usernames[i]+'?callback=?', function(data) {
-      if(data['error']){
-        var message = data['message'];
-        $('#link').prepend('<div><p>'+message+'</p></div>');
-      }
-      else{
-        var status = data['stream'];
-        var channel =  data['_links']['channel'];
-        var channelSplit = channel.split('/');
-        var channelName = channelSplit[5];
-        console.log(data);
-        console.log('status: ' + status);
-        console.log('user: ' + user);
-        if (status == null) {
-          $('#link').prepend('<div><p class="offline">Offline</p>'+channelName+'</div><hr />');
-        }
-        else{
-          var link = status.channel.url;
-          var name = status.channel.display_name;
-          var game = status.game;
-          var viewers = status.viewers;
+window.onload = function() {
+  var channels = ["ESL_DOTA2", "cretetion", "freecodecamp", "gamenewton", "scarra", "Doublelift", "adobe", "seebotschat"];
+  var divItem = document.getElementById("status");
 
-          $('#link').prepend('<div><p class="online">Online</p><p><a href="'+link+'" target="_blank">'+name+'</a><br />What\'s streaming: '+game+'<br /> Number of viewers: '+viewers+'</p></div>');
-        }
-      }
-    }).fail(function() {
-      $('#link').prepend('<div><p>Account doesn\'t exist');
-     });
+  function makeHttpObject() {
+    try {return new XMLHttpRequest();}
+    catch (error) {}
+    try {return new ActiveXObject("Msxml2.XMLHTTP");}
+    catch (error) {}
+    try {return new ActiveXObject("Microsoft.XMLHTTP");}
+    catch (error) {}
+
+    throw new Error("Could not create HTTP request object.");
   }
-})
+
+  function simpleHttpRequest(url, success, failure) {
+    var request = makeHttpObject();
+    request.open("GET", url, true);
+    request.responseType = "json";
+    request.send(null);
+    request.onreadystatechange = function() {
+      if (request.readyState == 4) {
+        if (request.status == 200) {
+          var data = request.response;
+          success(data);
+          var status = data.stream;
+          var link = data._links.channel;
+          //success(status);
+          var makeChild = document.createElement('div');
+          if (status == null) {
+            makeChild.innerHTML = '<div class="offline">'+link+'</div>';
+          } else {
+            makeChild.innerHTML = '<div class="online">'+link+'</div>';
+          }
+          divItem.appendChild(makeChild);
+        } else if (failure)
+          failure(request.status, request.statusText);
+      }
+    };
+  }
+
+  for (var i = 0; i < channels.length; i++) {
+    simpleHttpRequest("https://wind-bow.gomix.me/twitch-api/streams/"+channels[i], console.log);
+  }
+}
 
 function displayOnlineUsers() {
-
+  var onlineArray = document.getElementsByClassName("online");
+  var offlineArray = document.getElementsByClassName("offline");
+  for (var i = 0; i < onlineArray.length; i++) {
+    onlineArray[i].style.display = 'block';
+  }
+  for (var i = 0; i < offlineArray.length; i++) {
+    offlineArray[i].style.display = 'none';
+  }
 }
- /*{stream: null,
-  _links:
-    {self: "https://api.twitch.tv/kraken/streams/freecodecamp", channel: "https://api.twitch.tv/kraken/channels/freecodecamp"}}*/
+
+function displayOfflineUsers() {
+  var onlineArray = document.getElementsByClassName("online");
+  var offlineArray = document.getElementsByClassName("offline");
+  for (var i = 0; i < onlineArray.length; i++) {
+    onlineArray[i].style.display = 'none';
+  }
+  for (var i = 0; i < offlineArray.length; i++) {
+    offlineArray[i].style.display = 'block';
+  }
+}
+
+function displayAllUsers() {
+  var allArray = document.getElementsByClassName("users");
+  for (var i = 0; i < allArray.length; i++) {
+    allArray[i].style.display = 'block';
+  }
+}
